2648ea608cc041ad587de6edcb285162848a1262
unknown <kiratoka2@gmail.com>
2023-10-29 14:08:13 +0700

    no komen

--- src/components/AnimeDecs/CharaAnime.jsx
+++ src/components/AnimeDecs/CharaAnime.jsx
@@ -53,7 +53,7 @@ const CharaAnime = ({ api }) => {
     // Mengembalikan JSX
     return (
 
-
+        
 
         <div class="bg-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-6 gap-4">
             {/* <!-- Melakukan iterasi pada array data dan menampilkan elemen JSX untuk setiap objek --> */}
