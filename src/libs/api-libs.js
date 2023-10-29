acaad83d45158899b5cddd055176714c36c889bd
unknown <kiratoka2@gmail.com>
2023-10-29 16:19:18 +0700

    semoga work

--- src/libs/api-libs.js
+++ src/libs/api-libs.js
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic';
 export const getAnimeResponse = async (resource, query) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`, {cache:"no-store"})
     const anime = await response.json()
 
     return anime
