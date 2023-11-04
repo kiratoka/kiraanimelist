b5b7c678a731560283e781c3c382305a7037c452
unknown <kiratoka2@gmail.com>
2023-11-04 13:21:58 +0700

    menambahkan header banner Keqing

--- src/components/AnimeList/HeaderKeyword.jsx
+++ src/components/AnimeList/HeaderKeyword.jsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 const HeaderKeyword = ({ title, linkHref, linkTitle }) => {
   return (
     <div>
-      <div className='flex items-center h-52 bgHeaderPopuler mb-20 text-center justify-center'>
+      <div className='flex items-center h-52 bgKeqing mb-20 text-center justify-center'>
         <div className="flex backgroundTransparan2 h-64 items-center justify-center">
-          <h1 className='max-md:text-xl text-2xl font-bold text-color-primary text-center'>{title}</h1>
+          <h1 className='max-md:text-xl text-2xl font-bold text-white text-center '>{title}</h1>
           {
             linkHref && linkTitle
               ?
--- src/components/Utilities/HeaderMenu.jsx
+++ src/components/Utilities/HeaderMenu.jsx
@@ -1,9 +1,10 @@
 const HeaderMenu = ({ title }) => {
   return (
     <>
-      <div className="h-52 bgHeaderPopuler mb-20">
-        <div className="flex items-center justify-center backgroundTransparan2 h-64">
-          <h3 className="md:text-3xl text-2xl font-semibold text-center">{title}</h3>
+      <div className="h-52 bgKeqing mb-20">
+        <div className="flex items-center justify-center 
+         h-52 backgroundTransparan2">
+          <h3 className="md:text-3xl text-2xl font-semibold text-center text-white">{title}</h3>
         </div>
 
 
