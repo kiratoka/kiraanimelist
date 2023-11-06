bcdbc8472ee66cb8de2fab96864e738af7ade1fd
unknown <kiratoka2@gmail.com>
2023-11-06 14:12:51 +0700

    perbaikan kecil

--- src/components/Login/LoginAuth.jsx
+++ src/components/Login/LoginAuth.jsx
@@ -10,13 +10,13 @@ const LoginAuth = async () => {
         <div>
             <Link href={actionURL} className="flex justify-center text-center items-center bg-cyan-800 rounded-lg text-white hover:bg-cyan-900" >
 
-                <div className="flex flex-col justify-center py-3">
+                <div className="flex flex-col justify-center pt-3 pb-2">
                     <p>Masuk Menggunakan</p>
                     <div className="flex">
-                        <img src="https://cdn-icons-png.flaticon.com/512/281/281764.png" alt="" width="30px" className="mx-3 flex self-center py-3" />
+                        <img src="https://cdn-icons-png.flaticon.com/512/281/281764.png" alt="" width="20px" className="mx-3 flex self-center py-3" />
                         <p className="flex items-center">Google</p>
-                        <p className="flex items-center mx-3">atau</p>
-                        <img src="https://cdn-icons-png.flaticon.com/512/3291/3291695.png" alt="" width="30px" className="mr-3 flex self-center py-3" /> <p className="flex items-center">Github</p>
+                        <p className="flex items-center mx-2">atau</p>
+                        <img src="https://cdn-icons-png.flaticon.com/512/3291/3291695.png" alt="" width="20px" className="mr-3 ml-1 flex self-center py-3" /> <p className="flex items-center">Github</p>
 
                     </div>
                 </div>
