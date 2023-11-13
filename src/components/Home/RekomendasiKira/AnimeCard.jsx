da12d9f1d9b4903ea7cda54cab52f84c8a1574c8
unknown <kiratoka2@gmail.com>
2023-11-13 22:48:18 +0700

    perbaikan kecil lagi ini udah jam 22:48 WIB

--- src/components/Home/RekomendasiKira/AnimeCard.jsx
+++ src/components/Home/RekomendasiKira/AnimeCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const AnimeCard = ({ anime, onClick }) => {
   return (
-    <div className="anime-card flex justify-center max-lg:w-[10vw] max-lg:h-[14vh] max-xl:w-[11vw] max-xl:h-[20vh] h-[23.5vh] md:px-2" onClick={() => onClick(anime)}>
-        <img src={anime.portraitImage} alt={anime.name} className='flex justify-center cursor-pointer rounded object-cover hover:opacity-60 ' />
+    <div className="anime-card flex justify-center max-lg:w-[11vw] max-lg:h-[10vh] max-xl:w-[11vw] max-xl:h-[15vh] h-[23.5vh] md:px-2" onClick={() => onClick(anime)}>
+        <img src={anime.portraitImage} alt={anime.name} className='flex justify-center cursor-pointer rounded hover:opacity-60 ' />
     </div>
   );
 };
