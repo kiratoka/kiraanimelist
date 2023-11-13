03ab623b6e9aa7bfbdfa60c525341df9b26d31f3
unknown <kiratoka2@gmail.com>
2023-11-13 22:38:53 +0700

    responsifffff susah banget si

--- src/components/Home/RekomendasiKira/AnimeCard.jsx
+++ src/components/Home/RekomendasiKira/AnimeCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AnimeCard = ({ anime, onClick }) => {
   return (
-    <div className="anime-card flex justify-center h-[23.5vh] px-2" onClick={() => onClick(anime)}>
+    <div className="anime-card flex justify-center max-lg:w-[10vw] max-lg:h-[14vh] max-xl:w-[11vw] max-xl:h-[20vh] h-[23.5vh] md:px-2" onClick={() => onClick(anime)}>
         <img src={anime.portraitImage} alt={anime.name} className='flex justify-center cursor-pointer rounded object-cover hover:opacity-60 ' />
     </div>
   );
